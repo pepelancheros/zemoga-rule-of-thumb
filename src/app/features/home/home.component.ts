755f03c990c9200ad1c805dbe8463267aaba6f5b
pepelancheros <pepe.lancheros@gmail.com>
2020-02-09 11:15:48 -0500

    imports the data from the data.json file

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as data from '../../../incoming/data.json';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,20 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    getInfo();    
+    
   }
 
+}
+
+function getInfo() {
+  var name_space = document.getElementsByClassName('info__name');
+  var time_space = document.getElementsByClassName('info__time');
+  var description_space = document.getElementsByClassName('info__description');
+  for (var i=0; i<data.people.length; i++) {
+    var person = data.people[i];
+    name_space[i].innerHTML = person.name;
+    time_space[i].innerHTML = person.months + " in " + person.area;
+    description_space[i].innerHTML = person.description;
+  }
 }
