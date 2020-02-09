63a331b9ab638a72a6acff0c437552d369949a5f
pepelancheros <pepe.lancheros@gmail.com>
2020-02-09 18:55:25 -0500

    created the voting functions, working on console with localstorage

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -6,6 +6,7 @@ import * as data from '../../../incoming/data.json';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
@@ -13,7 +14,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     getInfo();  
     whiteBorder();  
-    
+    checkForVote();
   }
 
 }
@@ -22,11 +23,13 @@ function getInfo() {
   var name_space = document.getElementsByClassName('info__name');
   var time_space = document.getElementsByClassName('info__time');
   var description_space = document.getElementsByClassName('info__description');
+  var prueba_color = document.getElementsByClassName('llenar-color-dentro') as HTMLCollectionOf<HTMLElement>;
   for (var i=0; i<data.people.length; i++) {
     var person = data.people[i];
     name_space[i].innerHTML = person.name;
     time_space[i].innerHTML = person.months + " in " + person.area;
     description_space[i].innerHTML = person.description;
+    prueba_color[i].style.width = person.width;
   }
 }
 
@@ -42,4 +45,44 @@ function whiteBorder() {
       this.classList.remove('white-border');
     }
   }
+}
+
+function checkForVote() {
+  var btn_upvote = Array.prototype.slice.call(document.getElementsByClassName('thumb-up'));
+  var btn_downvote = Array.prototype.slice.call(document.getElementsByClassName('thumb-down'));
+  var btn_total = Array.prototype.slice.call(document.getElementsByClassName('btn-vote'));
+  var width_inicial = new Array(data.people.length);
+  var prueba_color = document.getElementsByClassName('llenar-color-dentro') as HTMLCollectionOf<HTMLElement>;
+
+  var local_edit = JSON.parse(localStorage.getItem('localfile'));
+
+  for (var i = 0; i < btn_upvote.length; i++) {
+    btn_upvote[i].addEventListener('click', upVote);
+    btn_downvote[i].addEventListener('click', downVote);
+    btn_total[i].addEventListener('click', sendVote);
+  }
+
+  function upVote() {
+    // prueba_color[btn_upvote.indexOf(this)].style.width = '50%';
+    var actual_index = btn_upvote.indexOf(this);
+    local_edit[actual_index]++;
+    console.log(local_edit[actual_index])
+    localStorage.setItem('localfile', JSON.stringify(local_edit));
+  }
+  function downVote() {
+    var actual_index = btn_downvote.indexOf(this);
+    local_edit[actual_index]--;
+    console.log(local_edit[actual_index])
+    localStorage.setItem('localfile', JSON.stringify(local_edit));
+  }
+  function sendVote() {
+    var local = localStorage.getItem('localfile');
+    console.log('retrievedObject: ', JSON.parse(local))
+  }
+
+  for (var i=0; i<data.people.length; i++) {
+    var person = data.people[i];
+    width_inicial[i] = person.width;
+  }
+  
 }
