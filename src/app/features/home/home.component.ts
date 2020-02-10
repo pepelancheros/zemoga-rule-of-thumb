2792677793d6d69615941b5b20cb91119a66abda
pepelancheros <pepe.lancheros@gmail.com>
2020-02-09 20:53:51 -0500

    corrected bugs that didn't let the votes count well, also created diferent localstorage files for upvotes and downvotes

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import * as data from '../../../incoming/data.json';
 
+
+var positive_votes = JSON.parse(localStorage.getItem('localfile-positive')) || new Array(data.people.length).fill(0);
+var negative_votes = JSON.parse(localStorage.getItem('localfile-negative')) || new Array(data.people.length).fill(0);
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -48,34 +52,55 @@ function checkForVote() {
   }
 
   function upVote() {
+    var actual_index = btn_upvote.indexOf(this);
+    if(btn_downvote[actual_index].classList.contains('clicked')) {
+      negative_votes[actual_index]--;
+    }
     // prueba_color[btn_upvote.indexOf(this)].style.width = '50%';
     if (!this.classList.contains('clicked')) {
       this.classList.add('clicked','white-border')
-      console.log('ya tiene la clase');
-      var actual_index = btn_upvote.indexOf(this);
-      local_edit[actual_index]++;
-      console.log(local_edit[actual_index])
-      localStorage.setItem('localfile', JSON.stringify(local_edit));
+      positive_votes[actual_index]++;
+      // console.log(positive_votes[actual_index]);
+      // console.log(getTotal(actual_index));
+      
       btn_downvote[actual_index].classList.remove('clicked','white-border');
     }
   }
   function downVote() {
+    var actual_index = btn_downvote.indexOf(this);
+    if(btn_upvote[actual_index].classList.contains('clicked')) {
+      positive_votes[actual_index]--;
+    }
     if (!this.classList.contains('clicked')) {
       this.classList.add('clicked','white-border')
-      console.log('ya tiene la clase el down');
-      var actual_index = btn_downvote.indexOf(this);
-      local_edit[actual_index]--;
-      console.log(local_edit[actual_index])
-      localStorage.setItem('localfile', JSON.stringify(local_edit));
+      negative_votes[actual_index]++;
+      // console.log(negative_votes[actual_index])
+      
       btn_upvote[actual_index].classList.remove('clicked','white-border');
     }
   }
   function sendVote() {
-    var local = localStorage.getItem('localfile');
-    console.log('retrievedObject: ', JSON.parse(local))
+    localStorage.setItem('localfile-positive', JSON.stringify(positive_votes));
+    localStorage.setItem('localfile-negative', JSON.stringify(negative_votes));
+    var local_positive = localStorage.getItem('localfile-positive');
+    var local_negative = localStorage.getItem('localfile-negative');    
+    console.log('positive: ', JSON.parse(local_positive));
+    console.log('negative: ', JSON.parse(local_negative));
     var actual_index = btn_total.indexOf(this);
     btn_downvote[actual_index].classList.remove('clicked','white-border');
     btn_upvote[actual_index].classList.remove('clicked','white-border');
+  }
+
+  function getTotal(elem) {
+    // var new_value = local_edit[elem];
+    // var total_sum = 0;
+    // var total_avg = 0;
+    // for (var i = 0; i < btn_upvote.length; i++) {
+    //   total_sum += local_edit[i];
+    // }
+    // total_avg = new_value / total_sum;
+
+    // return total_avg;
   }
 
   for (var i=0; i<data.people.length; i++) {
